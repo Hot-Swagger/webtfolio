78cd9104599c1340524979bc641889e3c25f72e3
user <user@DESKTOP-P8B9C8T>
2018-04-13 17:39:11 +0900

    mybatis config add

---
+++ src/main/java/com/webtfolio/config/MybatisConfig.java
@@ -0,0 +1,61 @@
+package com.webtfolio.config;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScans(value= {
+		@ComponentScan("com.webtfolio.dao"),
+		@ComponentScan("com.webtfolio.service")
+})
+public class MybatisConfig {
+	
+	@Bean(destroyMethod = "close")
+	public BasicDataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://211.238.142.97/newlecture?autoReconnect=true&amp;useSSL=false&amp;useUnicode=true&amp;characterEncoding=utf8");
+		dataSource.setUsername("sist");
+		dataSource.setPassword("cclass");
+		dataSource.setRemoveAbandoned(true);
+		dataSource.setInitialSize(20);
+		dataSource.setMaxActive(30);
+		return dataSource;
+	}
+		
+	@Bean
+	public SqlSessionFactoryBean sqlSessionFactory() {
+		
+		SqlSessionFactoryBean factory = new SqlSessionFactoryBean();
+		factory.setDataSource(dataSource());
+		factory.setMapperLocations(new Resource[] {
+				new ClassPathResource("com/webtfolio/dao/mybatis/mapper/*.xml")
+		});
+		return factory;
+	}
+	
+	@Bean
+	public SqlSessionTemplate sqlSession() throws Exception {
+		SqlSessionTemplate session = new SqlSessionTemplate(sqlSessionFactory().getObject());
+		return session;
+	}
+	
+	@Bean
+	public DataSourceTransactionManager transactionManager() {
+		DataSourceTransactionManager dataSourceTransactionManager
+			=	new DataSourceTransactionManager();
+		dataSourceTransactionManager.setDataSource(dataSource());
+		return dataSourceTransactionManager;
+	}
+}
